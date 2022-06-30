b375a47895d8319987dc12f14668ada188488ab8
Diogo RBG <diogorbg@gmail.com>
2022-06-30 11:16:59 -0300

    add moeda sfx
    set_deferred("monitoring", false) # não colidir mais de 1x (o personagem possui 2 colisores)

--- scripts/moeda.gd
+++ scripts/moeda.gd
@@ -3,8 +3,10 @@ extends Area2D
 
 onready var anim = $anim
 
-func _on_moeda_body_entered(body):
+func _on_moeda_body_entered(_body):
+	set_deferred("monitoring", false) # não colidir mais de 1x (o personagem possui 2 colisores)
+	$sfx.play()
 	anim.play("coletar")
 	yield(anim, "animation_finished") # aguarda a animação finalizar
 	queue_free() # destruição da moeda
-	print("destruido")
+	#print("destruido")
